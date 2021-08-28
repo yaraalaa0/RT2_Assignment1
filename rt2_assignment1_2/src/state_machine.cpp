2a13154ecd95b7d06152a29e49059089a7269db7
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2021-08-28 17:09:19 +0200

    Update state_machine.cpp

--- rt2_assignment1_2/src/state_machine.cpp
+++ rt2_assignment1_2/src/state_machine.cpp
@@ -6,6 +6,7 @@
 
 //add the ROS header
 #include "rclcpp/rclcpp.hpp"
+//header required for the component
 #include "rclcpp_components/register_node_macro.hpp"
 
 //add the headers for messages and services
@@ -27,6 +28,7 @@ namespace rt2_assignment1_2{
 class StateMachine : public rclcpp::Node
 {
 public:
+  //class constructor
   StateMachine(const rclcpp::NodeOptions & options)
   : Node("state_machine", options)
   { 
@@ -73,6 +75,7 @@ private:
   //function to call the random position service
   void call_server_rp(){
   
+  //define the minimum and maximum limits for the random position
   this->request_rp->x_max = 5;
   this->request_rp->x_min = -5;
   this->request_rp->y_max = 5;
@@ -99,6 +102,7 @@ private:
   //function to call the go to point service
   void call_server_p(){
   
+    //set the request to the last received random position response
     this->request_p->x = this->response_rp->x;
     this->request_p->y = this->response_rp->y;
     this->request_p->theta = this->response_rp->theta;
@@ -129,6 +133,8 @@ private:
     (void) request_header;
     
     RCLCPP_INFO(this->get_logger(), "UserInterfaceServer: received a request");
+    
+    //Check the command received from the client and update the flag accordingly
     if(request->command == "start"){
       this->start = true;
     }
@@ -146,14 +152,18 @@ private:
     if(this->start){
       
       //Check if the previous target has been reached or we are in the start of simulation
+      //In this case, call the random position server to get a new target position
       if(this->flag_rp==0 && this->flag_p){
+        //reset the flag to false in order not to execute this again
         this->flag_p = 0;
         //Request a new random position
         this->call_server_rp();
       }
       
       //Check if the random position client has received a response
+      //In this case, call the go_to_point server to drive the robot to the goal position
       if(this->flag_rp && this->flag_p == 0){
+        //reset the flag to false in order not to execute this again
         this->flag_rp = 0;
         //Call the go_to_point service
         this->call_server_p();
@@ -168,9 +178,9 @@ private:
   rclcpp::Client<RandomPosition>::SharedPtr client_rp; //random position client
   rclcpp::Client<Position>::SharedPtr client_p; //go to point client
   rclcpp::Service<Command>::SharedPtr service_;  //user interface server
-  std::shared_ptr<RandomPosition::Request> request_rp;
-  std::shared_ptr<RandomPosition::Response> response_rp;
-  std::shared_ptr<Position::Request> request_p;
+  std::shared_ptr<RandomPosition::Request> request_rp;  //the request variable to random position server
+  std::shared_ptr<RandomPosition::Response> response_rp;  //the response variable to random position server
+  std::shared_ptr<Position::Request> request_p;  //the request variable to go_to_point server
   
   bool start = false; //the start flag to indicate the user command
   int flag_rp = 0; //flag to indicate receiving a random position response
