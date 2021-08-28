e32ebfbc5ad0d9ae0ea65fbda2106c905b441855
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2021-08-28 17:16:57 +0200

    Update position_service.cpp

--- rt2_assignment1_2/src/position_service.cpp
+++ rt2_assignment1_2/src/position_service.cpp
@@ -17,30 +17,36 @@ namespace rt2_assignment1_2{
 class RandPosServer : public  rclcpp::Node
 {
 public:
+  //class constructor
   RandPosServer(const rclcpp::NodeOptions & options)
   : Node("random_position_server", options)
   {
+    //Create the server of the random position service /position_server
     service_r = this->create_service<RandomPosition>("/position_server", std::bind(&RandPosServer::myrandom, this, _1, _2, _3));
   
   }
 private:
+  //A function to generate a random number between a minimum and maximum value
   double randMToN(double M, double N){     
   return M + (rand() / ( RAND_MAX / (N-M) ) ); 
   }
   
+  //The callback to the server service_r
   bool myrandom (const std::shared_ptr<rmw_request_id_t> request_header,
   const std::shared_ptr<RandomPosition::Request> request,
   const std::shared_ptr<RandomPosition::Response> response)
   {
     (void) request_header;
+    //Get the random x, y, and theta values in the requested ranges
     response->x = this->randMToN(request->x_min, request->x_max);
     response->y = this->randMToN(request->y_min, request->y_max);
     response->theta = this->randMToN(-3.14, 3.14);
+    //Print info on screen
     RCLCPP_INFO(this->get_logger(), "RandomPositionServer: Received a request");
     return true;
 }
 
-  rclcpp::Service<RandomPosition>::SharedPtr service_r;
+  rclcpp::Service<RandomPosition>::SharedPtr service_r;   //Initialize the server to the random position service
 
 };
 
@@ -48,12 +54,3 @@ private:
 
 RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1_2::RandPosServer) 
 
-/*
-int main(int argc, char **argv)
-{
-   rclcpp::init(argc, argv);
-   auto node = std::make_shared<RandPosServer>();
-   rclcpp::spin(node);
-
-   return 0;
-}*/
